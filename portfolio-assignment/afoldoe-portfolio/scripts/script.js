99d527a047fa2befc5c13b4f1edae5d036d99532
Arielle Foldoe <afoldoe@gmail.com>
2016-05-15 14:42:10 -0700

    class 07 port assignment

--- portfolio-assignment/afoldoe-portfolio/scripts/script.js
+++ portfolio-assignment/afoldoe-portfolio/scripts/script.js
@@ -10,13 +10,21 @@ function Project (opts) {
 }
 
 Project.prototype.toHtml = function() {
+  var templateScript = $('#project-template').html();
+  var finalTemplate = Handlebars.compile(templateScript);
+
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? this.daysAgo + ' days ago' : '(draft)';
+
+  return finalTemplate(this);
+  console.log(finalTemplate);
+
+  /*
   var $newProject = $('article.template').clone();
   if (!this.publishedOn) {
     $newProject.addClass('draft');
   }
   $newProject.attr('data-category', this.category);
-  // : Use jQuery to also add the author name as a data-attribute of the newly cloned article.
-  //       Doing so will allow us to use selectors to target articles, based on who wrote them.
   $newProject.attr('data-author', this.author);
   $newProject.find('.byline a').html(this.author);
   $newProject.find('header a').attr('href', this.projectURL);
@@ -28,6 +36,7 @@ Project.prototype.toHtml = function() {
   $newProject.append('<hr>');
   $newProject.removeClass('template');
   return $newProject;
+  */
 }
 
 projectData.sort(function(a,b) {
